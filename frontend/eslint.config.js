06c5f432157bf4811c0df3759f8d27fc2449deef
Egor <egoryarovitsyn1@gmail.com>
2025-10-06 21:40:39 +0300

    init frontend

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,23 +1,38 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
-
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "simple-import-sort", "prettier"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
     ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
+    "simple-import-sort/imports": [
+      "error",
+      {
+        groups: [
+          ["^react"],
+          ["@reduxjs/toolkit"],
+          ["^react-router-dom", "^react-router"],
+          [
+            "^@/app",
+            "^@/pages",
+            "^@/widgets",
+            "^@/features",
+            "^@/entities",
+            "^@/shared",
+          ],
+        ],
+      },
+    ],
+    "prettier/prettier": "error",
   },
-])
+};
